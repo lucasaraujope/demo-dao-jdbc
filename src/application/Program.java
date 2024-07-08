b84bae0c9a95f52c8f24625274008392d8a9331a
Lucas Araujo <lucas.araujope22@gmail.com>
2024-07-08 09:42:45 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department obj = new Department(1, "Book");
+		
+		Seller seller = new Seller(15, "Lucas", "Lucas@gmail.com", new Date(), 3000.0 , obj);
+		System.out.println(seller);
 	}
 }
