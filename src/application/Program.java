853c1dd3873ff3bb9d48a1fbd5e165a35b4f5abd
Lucas Araujo <lucas.araujope22@gmail.com>
2024-07-14 04:48:32 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -11,9 +9,10 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Book");
 		
-		Seller seller = new Seller(15, "Lucas", "Lucas@gmail.com", new Date(), 3000.0 , obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);	
+		
 		System.out.println(seller);
 	}
 }
